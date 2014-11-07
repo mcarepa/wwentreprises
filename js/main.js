22dff37bab5f8864f48dd4a471e235b608175370
mcarepa <sokratchet@gmail.com>
2014-11-07 01:38:22 +0000

    left-menu bar handle movement

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(function() {
 	setupLandingPage();
+	$(".brand-logo").click(function(){
+		handleLeftMenu()
+	});
+})
+
+$(window).resize(function() {
+	setupLandingPage();
 })
 
 function setupLandingPage() {
@@ -7,4 +14,24 @@ function setupLandingPage() {
 	var marginTop = (height/2) - ($(".splash-text").height()/2);
 	$(".landing-image-wrap").css("height",height);
 	$(".splash-text").css("margin-top", marginTop)
+}
+
+function handleLeftMenu() {
+	$menu = $(".fixed-nav .left-menu");
+	$logoWrap = $(".fixed-nav .logo-wrap");
+	$splashText = $(".landing-image-wrap .splash-text");
+	var width = $menu.width();
+	var visible = ($menu.offset().left == -width) ? false:true;
+	switch(visible) {
+		case true:
+			$menu.animate({"left": -width},300);
+			$logoWrap.animate({"margin-left": $logoWrap.offset().left - width}, 300);
+			$splashText.animate({"margin-left": 0}, 300);
+			break;
+		case false:
+			$menu.animate({"left": 0},300);
+			$logoWrap.animate({"margin-left": $logoWrap.offset().left + width}, 300);
+			$splashText.animate({"margin-left": width}, 300);
+			break;
+	}
 }
