b0feb7fee3a8370f0477b2e5480d3161a678fc37
mcarepa <sokratchet@gmail.com>
2014-11-07 02:30:13 +0000

    Top bar animation / left menu btn

--- js/main.js
+++ js/main.js
@@ -1,25 +1,41 @@
 $(function() {
 	setupLandingPage();
-	$(".brand-logo").click(function(){
+	$(".left-menu-btn").click(function(){
 		handleLeftMenu()
 	});
+
 })
 
 $(window).resize(function() {
 	setupLandingPage();
 })
 
+var animate = true;
+
+$(window).scroll(function(){
+	var scroll = $(window).scrollTop();
+	if(scroll >= 100 && animate) {
+		$(".fixed-nav .top-menu").animate({backgroundColor: jQuery.Color("rgba(48,60,73,0.6)")},350);
+		animate = false;
+	}
+	else if (scroll < 100 && !animate) {
+		$(".fixed-nav .top-menu").animate({backgroundColor: jQuery.Color("rgba(48,60,73,0)")},350);
+		animate = true;
+	}
+})
+
 function setupLandingPage() {
 	var height = $(window).height();
 	var marginTop = (height/2) - ($(".splash-text").height()/2);
 	$(".landing-image-wrap").css("height",height);
-	$(".splash-text").css("margin-top", marginTop)
+	$(".splash-text").css("margin-top", marginTop).delay(100).fadeIn(500);
 }
 
 function handleLeftMenu() {
 	$menu = $(".fixed-nav .left-menu");
 	$logoWrap = $(".fixed-nav .logo-wrap");
 	$splashText = $(".landing-image-wrap .splash-text");
+	$topMenuBtn = $(".fixed-nav .top-menu .left-menu-btn");
 	var width = $menu.width();
 	var visible = ($menu.offset().left == -width) ? false:true;
 	switch(visible) {
@@ -27,11 +43,13 @@ function handleLeftMenu() {
 			$menu.animate({"left": -width},300);
 			$logoWrap.animate({"margin-left": $logoWrap.offset().left - width}, 300);
 			$splashText.animate({"margin-left": 0}, 300);
+			$topMenuBtn.fadeIn(300);
 			break;
 		case false:
 			$menu.animate({"left": 0},300);
 			$logoWrap.animate({"margin-left": $logoWrap.offset().left + width}, 300);
 			$splashText.animate({"margin-left": width}, 300);
+			$topMenuBtn.fadeOut(300);
 			break;
 	}
 }
